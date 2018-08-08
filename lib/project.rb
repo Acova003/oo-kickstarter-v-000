80d90df99b10664976c11899336a6975085802c2
Amee Covarrubias <ameecova@yahoo.com>
2018-08-08 14:07:20 +0000

    automatically backed up by learn

---
+++ lib/project.rb
@@ -0,0 +1,14 @@
+require 'pry'
+class Project 
+  attr_accessor :backers, :title
+  
+  def initialize(title)
+    @title = title 
+    @backers = []
+  end 
+  
+  def add_backer(title)
+    #binding.pry
+    @backers << @title
+  end 
+end 
